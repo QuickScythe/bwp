7ba7058239e94707957c7f175e74fe79bbb98ae7
QuickScythe <quickscythe@gmail.com>
2025-09-28 23:51:21 -0500

    init commit 9

--- src/main/java/com/bwp/server/PageController.java
+++ src/main/java/com/bwp/server/PageController.java
@@ -1,24 +1,14 @@
 package com.bwp.server;
 
-import com.bwp.Main;
 import com.bwp.data.Actor;
 import com.bwp.data.config.Configs;
 import com.bwp.data.config.TalentConfig;
-import com.bwp.utils.Utils;
-import com.bwp.utils.sql.SqlDatabase;
-import com.bwp.utils.sql.SqlUtils;
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
