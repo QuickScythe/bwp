a53b3b17876001a0a7528073e9916d22e34f3e17
QuickScythe <quickscythe@gmail.com>
2025-09-30 19:32:14 -0500

    dev test 10

--- src/test/java/com/bwp/tests/Generation.java
+++ src/test/java/com/bwp/tests/Generation.java
@@ -10,19 +10,19 @@ public class Generation {
 
     @Test
     public void generate() {
-        Main.Integration integration = new Main.Integration();
-        Utils.init(integration);
-        TalentConfig talentConfig = Configs.talent();
-        talentConfig.actor(20819);
-        talentConfig.actor(154182);
-        talentConfig.actor(1214974);
-        talentConfig.actor(15516);
-        talentConfig.actor(224526);
-        talentConfig.actor(1230724);
-        talentConfig.actor(29479);
-        talentConfig.actor(100085);
-        talentConfig.actor(223050);
-        talentConfig.actor(4164165);
-        talentConfig.save();
+//        Main.Integration integration = new Main.Integration();
+//        Utils.init(integration);
+//        TalentConfig talentConfig = Configs.talent();
+//        talentConfig.actor(20819);
+//        talentConfig.actor(154182);
+//        talentConfig.actor(1214974);
+//        talentConfig.actor(15516);
+//        talentConfig.actor(224526);
+//        talentConfig.actor(1230724);
+//        talentConfig.actor(29479);
+//        talentConfig.actor(100085);
+//        talentConfig.actor(223050);
+//        talentConfig.actor(4164165);
+//        talentConfig.save();
     }
 }
